9b777fb24c22a3e7bb063588a1d03ccd6dc4de26
Alex Strigelskiy <avstri@gmail.com>
2023-10-04 13:31:43 -0400

    missing some files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,27 +1,52 @@
 from django.db import models
 from django.utils.timezone import now
 
-
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=20)
+    origin_country = models.CharField(max_length=50)
+    description = models.TextField(max_length=100)
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+    def __str__(self):
+        return self.name
 
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    WAGON = 'Wagon'
+    SUV = 'SUV'
+    COUPE = 'Coupe'
+    MINIVAN = 'MiniVan'
+    TRUCK = 'Truck'
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (WAGON, 'Wagon'),
+        (SUV, 'SUV'),
+        (COUPE, 'Coupe'),
+        (MINIVAN, 'MiniVan'),
+        (TRUCK, 'Truck')
+    ]
+
+    name = models.CharField(max_length=20)
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    car_type = models.CharField(
+        null = False,
+        max_length=20,
+        choices = CAR_TYPE_CHOICES,
+        default = SEDAN)
+    year = models.DateField(null = False)
+    dealerId = models.IntegerField(null = True)
+
+    def __str__(self):
+        return f"{self.make} {self.name} {self.year}"
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+# class CarDealer(models.Model):
+#     def __str__(self):
+#         return "car Dealer"
 
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+# # <HINT> Create a plain Python class `DealerReview` to hold review data
+# class DealerReview(models.Model):
+#     def __str__(self):
+#         return "car Dealer Review"
